96b04c11983c7d0a7e6287dfc76e75d9a9832dc3
Anushri <sastikaranushri@gmail.com>
2025-11-29 17:33:07 -0500

    optimal solution

--- search_a_2d_matrix.py
+++ search_a_2d_matrix.py
@@ -1,29 +1,30 @@
 # https://leetcode.com/problems/search-a-2d-matrix-ii/
 
-# brutforce O(m*n)
+# To find the target element, start checking from the top left corner of the matrix. If the target is less than the current element decrease the column as the matrix is sorted. and if it's greater than the current element increase the row.
+# Time Complexity- O(m + n) Space Complexity- O(1)
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
         m = len(matrix)
         n = len(matrix[0])
-        # apply binary search on rows
-        # same can be done on columns using 
-        # for i in range(n)
-            # low, high = 0, m - 1
-        for i in range(m):
-            low, high = 0, n - 1
-            while low <= high:
-                mid = low + (high - low) // 2
-                if matrix[i][mid] == target:
-                    return True
-                elif matrix[i][mid] < target:
-                    low = mid + 1
-                else:
-                    high = mid - 1
+        # starting from the top-right corner
+        r, c = 0 , n - 1
+        
+        while r < m and c >= 0:
+            # if the target is present at this coordinate
+            if matrix[r][c] == target:
+                return True
+            # if the target is less than the element
+            elif matrix[r][c] > target:
+                # decrement the column
+                c -= 1
+            else:
+                # increment the row 
+                r += 1
 
-        return n
+        return False
         
 solution = Solution()
-print(solution.searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5))
+print(solution.searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 19))
 
 # target = 5
 # output = true
